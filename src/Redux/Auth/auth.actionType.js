b83f9e68a6d061ebdd8203b5621dfaca4f154f90
Mohanadithya4455 <mohanrankireddy4455@gmail.com>
2024-11-04 10:25:58 +0530

    frontend deleted

--- src/Redux/Auth/auth.actionType.js
+++
@@ -1,6 +0,0 @@
-export const LOGIN_SUCCESS = "Login_Success";
-export const LOGIN_FAILURE = "Login_Failure";
-export const LOGIN_REQUEST = "Login_Request";
-export const REGISTER_REQUEST = "Register_Request";
-export const REGISTER_SUCCESS = "Register_Success";
-export const REGISTER_FAILURE = "Register_Failure";
--- src/Redux/store.js
+++
@@ -1,8 +0,0 @@
-import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
-import { thunk } from "redux-thunk";
-import { authReducers } from "./Auth/auth.reducers";
-
-const allReducers = combineReducers({
-auth:authReducers
-})
-export const store=legacy_createStore(allReducers,applyMiddleware(thunk));
